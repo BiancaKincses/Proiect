fc23e630bb697691a526d39de49aa03b75ba64d7
Kincses Bianca <kincsesbianca4@gmail.com>
2023-01-16 18:56:58 +0200

    Add project files.

---
+++ ProductPage.xaml.cs
@@ -0,0 +1,48 @@
+using Proiect.Models;
+
+namespace Proiect;
+
+public partial class ProductPage : ContentPage
+{
+    ShopList s1;
+	public ProductPage(ShopList slist)
+	{
+		InitializeComponent();
+        s1 = slist;
+	}
+    async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        Product p;
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            var lp = new ListProduct()
+            {
+                ShopListID = s1.ID,
+                ProductID = p.ID
+            };
+            await App.Database.SaveListProductAsync(lp);
+            p.ListProducts = new List<ListProduct> { lp };
+            await Navigation.PopAsync();
+        }
+    }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.SaveProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var product = (Product)BindingContext;
+        await App.Database.DeleteProductAsync(product);
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetProductsAsync();
+    }
+
+}
